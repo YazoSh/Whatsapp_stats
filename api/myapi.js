f7704b0cf3cc4d9a755d1873e8d979272bfe47fa
Yazan Alshrouf <yazo_sh@yahoo.com>
2022-01-03 17:56:02 +0200

    rewrote helper functions

--- api/myapi.js
+++ api/myapi.js
@@ -1,9 +1,3 @@
-async function getAPI(url, callback) {
-	fetch(url)
-		.then(resp => resp.json())
-		.then(data => callback(data)); //Replace with whatever
-};
-
 async function postAPI(url, file, callback) {
 		formData = new FormData();
 		formData.set('file', file, 'file');
--- server/helpers.py
+++ server/helpers.py
@@ -1,62 +1,31 @@
 import re
 
-def startsWithDateAndTime(s):
-	pattern = '\[([0-9]+(\.[0-9]+)+),*.(([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[eE]([+-]?\d+))?(:([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[eE]([+-]?\d+))?)+)*.[a-zA-Z]+]'
-	result = re.match(pattern, s)
-	if result:
-		return True
-	return False
-
-def getDateOfMessage(s):
-	pattern = '([0-9]+(\.[0-9]+)+)'
-	result = re.compile(pattern)
-	return result.search(s).group(1)
-
-def getName(string):
-	withOutDate = re.sub("[\(\[].*?[\)\]]", "", string)
-	newText = []
-	for i in withOutDate:
-		if i == ' ' or i == '\u200e':
-			continue
-		if i != ':':
-			newText.append(i)
-		else:
-			return ''.join(newText)
-
-def findDateInData(date, data):
-	for x in range(len(data)):
-		if data[x]['date'] == date:
-			return x
-	return None
-
-
 def sendData(file):
-	line_count = 0
-	usersCount = {}
+	pattern = re.compile(r'^\[(\d+\.\d+\.\d+), .+\]\s+([^:]+):\s*(.*)', re.M)
+
 	data = []
+
+	prevday = None
+
 	for line in file:
 		if type(line) is bytes:
 			line = line.decode("utf-8")
-			
-		if startsWithDateAndTime(line):
-			dateOfMsg = getDateOfMessage(line)
-			user = getName(line)
-			dateIndex = findDateInData(dateOfMsg, data)
-			if dateIndex is not None:
-				try:
-					data[dateIndex][user] += 1
-				except KeyError:
-					data[dateIndex][user] = 1
-			else:
-				toData = {
-					"date": dateOfMsg,
-				}
-				toData[user] = 1
-				data.append(toData)
-			line_count += 1
 
-			try:
-				usersCount[user] += 1
-			except KeyError:
-				usersCount[user] = 1
+		matches = pattern.findall(line)
+
+		for entry in matches:
+			(date, name, msg) = entry
+
+			if prevday and prevday["date"] == date:
+				if name in prevday["people"]:
+					prevday["people"][name] += 1
+				else:
+					prevday["people"][name] = 1
+			else:
+				prevday = {
+					"date": date,
+					"people": {},
+				}
+				prevday["people"][name] = 1
+				data.append(prevday)
 	return data
